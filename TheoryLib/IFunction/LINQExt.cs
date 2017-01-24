1c8f5a4d6029275d261481656be5bd1fc4238c4a
John Hollett <g1xb17@gmail.com>
2017-01-24 17:22:42 -0330

    Fixes

--- TheoryLib/IFunction/LINQExt.cs
+++ TheoryLib/IFunction/LINQExt.cs
@@ -10,19 +10,19 @@ namespace TheoryLib.IFunction {
             if( ( src.Count() != other.Count() ) || ( src.Count() == 0 ) ) {
                 throw new InvalidOperationException( "Empty set or unequal sizes" );
             }
-            System.Collections.IEnumerator A = src.GetEnumerator();
-            System.Collections.IEnumerator B = other.GetEnumerator();
+            var A = src.GetEnumerator();
+            var B = other.GetEnumerator();
             while( A.MoveNext() && B.MoveNext() ) {
                 yield return operation( ( TFirst )A.Current , ( TSecond )B.Current );
             }
         }
 
         public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>( this IEnumerable<TFirst> src , IEnumerable<TSecond> other , Func<TFirst , TSecond , TResult> operation ) {
-            var A = src.GetEnumerator();
-            var B = other.GetEnumerator();
             if( src.Count() != other.Count() || src.Count() == 0 ) {
                 throw new InvalidOperationException( "Empty Set or unequal sizes" );
             }
+            var A = src.GetEnumerator();
+            var B = other.GetEnumerator();
             while( A.MoveNext() && B.MoveNext() ) {
                 yield return operation( A.Current , B.Current );
             }
